d04705b4df61a8a11acfcd7fdcaac9258cdb1146
Sanae Aoki <134927378+sa-aoki@users.noreply.github.com>
2023-06-12 11:19:49 +0900

    event.jaアップロード

--- kadai_019/event.js
+++ kadai_019/event.js
@@ -1,1 +1,6 @@
+const clickBtn = document.getElementById('btn');
 
+clickBtn.addEventListener('click',() => {
+  const Changetext = document.getElementById('text');
+  Changetext.innerHTML = 'ボタンをクリックしました';
+});
